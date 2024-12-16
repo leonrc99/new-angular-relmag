72bd9a4a42c98430309d4eb34e4a2876af31651d
Leonardo Rodrigues <leonardo.rc999@gmail.com>
2024-12-16 16:49:56 -0300

    create admin and consultant dashboard

---
+++ src/app/pages/consultant-dashboard/consultant-dashboard.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-consultant-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './consultant-dashboard.component.html',
+  styleUrl: './consultant-dashboard.component.scss'
+})
+export class ConsultantDashboardComponent {
+
+}
